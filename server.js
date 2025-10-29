4aee8214d0da2e1170607634e95da3ee6efa7dea
kotomaricodes <101243546+kotomaricodes@users.noreply.github.com>
2025-10-29 18:52:20 -0400

    Create server.js

---
+++ server.js
@@ -0,0 +1,47 @@
+import express from "express";
+import { createServer } from "http";
+import { Server } from "socket.io";
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const app = express();
+const server = createServer(app);
+const io = new Server(server);
+
+const PORT = process.env.PORT || 10000;
+
+app.use(express.static(path.join(__dirname, "public")));
+
+const questions = [
+  ["Be able to fly", "Be invisible"],
+  ["Never need sleep", "Never need food"],
+  ["Live 100 years in the future", "Live 100 years in the past"]
+];
+
+let currentQuestion = 0;
+let votes = { A: 0, B: 0 };
+
+io.on("connection", (socket) => {
+  console.log("New player connected");
+  socket.emit("question", questions[currentQuestion]);
+
+  socket.on("vote", (choice) => {
+    votes[choice]++;
+    io.emit("results", votes);
+  });
+
+  socket.on("next", () => {
+    currentQuestion = (currentQuestion + 1) % questions.length;
+    votes = { A: 0, B: 0 };
+    io.emit("question", questions[currentQuestion]);
+  });
+
+  socket.on("disconnect", () => {
+    console.log("Player disconnected");
+  });
+});
+
+server.listen(PORT, () => console.log(`Server running on ${PORT}`));
